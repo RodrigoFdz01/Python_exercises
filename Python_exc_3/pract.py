e26d0be8104c0af446befeb3981fc87866ea3587
RodrigoFdz01 <rodrigof3012@gmail.com>
2023-01-17 13:18:35 -0300

    excer add...

--- Python_exc_3/pract.py
+++ Python_exc_3/pract.py
@@ -25,7 +25,14 @@ count += 1
 print("igualaste la extension")
 
 
-'''
+###   Debes crear una funcion llamada `sumattion` que reciba un numero como parametro y
+que devuelva la sumatoria de todos sus numeros anteriores, incluso ese mismo.
+Ejemplo:
+- sumattion(3) debe retornar 6 porque hace (1+2+3)
+- sumattion(8) debe retornar 36  
+
+
+
 def sumattion(number):
     summation = 0
     limit = number +1
@@ -34,5 +41,23 @@ def sumattion(number):
     print(summation) 
 
 sumattion(4)
+'''
 
 
+'''
+Crea una aplicacion qu obtenga los elementos impares de inalista pasada por parametro con filter
+y realice una suma de esos datos obtenidos mediante reduce
+
+'''
+
+lista_de_numeros= [1,2,3,4,5,6]
+
+# filter
+
+impares = list(filter(lambda el : el % 2 != 0 , lista_de_numeros))
+print(impares)
+
+
+from functools import reduce
+suma = reduce(lambda a, b : a + b, impares)
+print(suma)
