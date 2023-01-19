32a7c8d2f0a069e0194c23aac368409e97aa0647
RodrigoFdz01 <rodrigof3012@gmail.com>
2023-01-18 23:58:53 -0300

    new folder

--- Python_exc_3/pract.py
+++ Python_exc_3/pract.py
@@ -30,18 +30,16 @@ que devuelva la sumatoria de todos sus numeros anteriores, incluso ese mismo.
 Ejemplo:
 - sumattion(3) debe retornar 6 porque hace (1+2+3)
 - sumattion(8) debe retornar 36  
-
-
+'''
 
 def sumattion(number):
     summation = 0
     limit = number +1
     for i in range(1,limit):
         summation = summation + i
-    print(summation) 
+    #print(summation) 
 
 sumattion(4)
-'''
 
 
 '''
@@ -55,9 +53,74 @@ lista_de_numeros= [1,2,3,4,5,6]
 # filter
 
 impares = list(filter(lambda el : el % 2 != 0 , lista_de_numeros))
-print(impares)
+#print(impares)
 
 
 from functools import reduce
 suma = reduce(lambda a, b : a + b, impares)
-print(suma)
+#print(suma)
+
+
+### Implementar la clase Libro q tenga info de su autor, titulo del libro, 
+# genero  cantidad de paginas.
+### INcluir todos los comandos de consulta y modificacion de atributos(los get y set)
+### Implementar los metedos equals, copy y clone.
+### MOstrar ejemplos de uso de cada uno para verifcar el funcionamiento
+
+
+class Libro:
+    def __init__(self,autor=None,titulo=None,genero=None,paginas=None):
+        self.autor = autor
+        self.titulo = titulo
+        self.genero = genero
+        self.paginas =  paginas
+
+    def get_autor(self):
+        return self.autor
+
+    def get_titulo(self):
+        return self.titulo
+
+    def get_genero(self):
+        return self.genero
+
+    def get_paginas(self):
+        return self.paginas
+
+    ####
+    def set_autor(self,autor):
+        self.autor = autor
+
+    def settitulo(self,titulo):
+        self.titulo =titulo
+
+    def set_genero(self,genero):
+        self.genero = genero
+
+    def set_paginas(self,paginas):
+        self.paginas = paginas
+
+
+    def clone(self):
+        return Libro(self.autor,self.titulo,self.genero,self.paginas)
+
+    
+    def copy(self):
+        return self
+
+    def equals(self, objeto):
+        return True if self is objeto else False
+
+
+libro1 = Libro("Juan", "El resplandor", "Terror","1501")
+print(libro1)
+
+libro_clone = libro1.clone()
+#print(libro_clone)
+
+libro_copia = libro1.copy()
+#print(libro_copia)
+
+# metodo equals , chequea si un obj es el mismo
+print(libro1.equals(libro_clone))
+print(libro1.equals(libro_copia))
